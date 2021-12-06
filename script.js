bcbd24f9cf873937c5a2890f948804d891a3a93c
Amram1997 <amramananyan9@gmail.com>
2021-12-06 22:10:19 +0400

    feat

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 // 1. Create a function that implements partial sum
 function sum(a){
+  
     return function(b){
         return a + b
     }
 }
 console.log(sum(1)(2))
+ 
+const addOne = sum(1);
+console.log(addOne(2))
+const addTen = sum(10);
+console.log(addTen(2));
+console.log(addOne(4));
+console.log(addTen(10))
