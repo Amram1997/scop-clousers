3b54cab3d7b6a8f34c9a071ea65ae7d628b80050
Amram1997 <amramananyan9@gmail.com>
2021-12-07 12:58:46 +0400

    feat

---
+++ script3.js
@@ -0,0 +1,10 @@
+// 4. Create a function printAfter that calls its argument after printing hello world
+const print = () => "Elon Musk"
+
+ function printAfter(pr){
+        console.log("hello world")
+          return pr()
+         
+     
+ }
+ console.log(printAfter(print))
